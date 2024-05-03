fe87b34dca59dfc8eba1751daa7f72ff2f268c21
raphaeltkorp <raphael@tkorp.com>
2024-05-03 23:20:43 +0200

    Add analytics and robots txt

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,32 @@
+<script lang="js">
+  import { page } from '$app/stores'
+
+ // @ts-ignore
+   $: {
+    // @ts-ignore
+    if (typeof gtag !== 'undefined') {
+      // @ts-ignore
+      gtag('config', 'G-98S1QD8H2L', {
+        page_title: document.title,
+        page_path: $page.url.pathname,
+      })
+    }
+  }
+</script>
+
+<svelte:head>
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-98S1QD8H2L">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || []
+
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', 'G-98S1QD8H2L')
+  </script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import Analytics from '$lib/analytics.svelte';
 </script>
+
+<Analytics />
 
 <slot />
