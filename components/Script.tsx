6426ef666f8c2cd272cbc1a803915bb13030479e
raphplt <raphael.plassart@gmail.com>
2025-02-16 14:05:05 +0100

    feat: Update Google Analytics integration to use environment variable for tracking ID and enhance privacy settings

--- components/Script.tsx
+++ components/Script.tsx
@@ -3,21 +3,25 @@ import React from "react";
 import Script from "next/script";
 
 const Scripte = () => {
+	const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_ID;
+
 	return (
 		<>
 			<Script
 				strategy="afterInteractive"
-				src={`https://www.googletagmanager.com/gtag/js?id=G-98S1QD8H2L`}
+				src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
 			/>
 			<Script id="google-analytics" strategy="afterInteractive">
 				{`
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', 'G-98S1QD8H2L', {
-						page_path: window.location.pathname,
-						});
-					`}
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${googleAnalyticsId}', {
+                        page_path: window.location.pathname,
+                        anonymize_ip: true,
+                        storage: 'none'              
+                    });
+                    `}
 			</Script>
 		</>
 	);
