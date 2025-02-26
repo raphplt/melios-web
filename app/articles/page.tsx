c6c7b503d7d7b78a7cfee6c18ae099cfb676a264
raphaeltkorp <raphael@tkorp.com>
2025-02-27 00:36:18 +0100

    feat: add cover image for blog post and improve article loading logic

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -7,93 +7,94 @@ import { Button, Spinner } from "@heroui/react";
 import { PostPreviewCompact } from "@/components/blog/PostPreviewCompact";
 
 export default function Index() {
-	const [page, setPage] = useState(1);
-	const [allPosts, setAllPosts] = useState<Post[]>([]);
-	const [hasMorePosts, setHasMorePosts] = useState(true);
-	const [loadedSlugs, setLoadedSlugs] = useState<Set<string>>(new Set());
+    const [page, setPage] = useState(1);
+    const [allPosts, setAllPosts] = useState<Post[]>([]);
+    const [hasMorePosts, setHasMorePosts] = useState(true);
+    const [loadedSlugs, setLoadedSlugs] = useState<Set<string>>(new Set());
 
-	useEffect(() => {
-		const fetchPosts = async () => {
-			const res = await fetch(`/api/posts?page=${page}`);
-			const newPosts = await res.json();
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await fetch(`/api/posts?page=${page}`);
+            const newPosts = await res.json();
 
-			const uniquePosts = newPosts.filter(
-				(post: Post) => !loadedSlugs.has(post.slug)
-			);
-			setAllPosts((prevPosts) => [...prevPosts, ...uniquePosts]);
-			setLoadedSlugs((prevSlugs) => {
-				const newSlugs = new Set(prevSlugs);
-				uniquePosts.forEach((post: Post) => newSlugs.add(post.slug));
-				return newSlugs;
-			});
+            const uniquePosts = newPosts.filter(
+                (post: Post) => !loadedSlugs.has(post.slug)
+            );
+            setAllPosts((prevPosts) => [...prevPosts, ...uniquePosts]);
+            setLoadedSlugs((prevSlugs) => {
+                const newSlugs = new Set(prevSlugs);
+                uniquePosts.forEach((post: Post) => newSlugs.add(post.slug));
+                return newSlugs;
+            });
 
-			if (newPosts.length === 0 || newPosts.length < 10) {
-				setHasMorePosts(false);
-			}
-		};
+            if (newPosts.length === 0 || newPosts.length < 10) {
+                setHasMorePosts(false);
+            }
+        };
 
-		fetchPosts();
-	}, [page]);
+        fetchPosts();
+    }, [page]);
 
-	const heroPost = allPosts[0];
-	const morePosts = allPosts.slice(1);
+    const heroPost = allPosts[0];
+    const morePosts = allPosts.slice(1);
 
-	const pinnedPosts = morePosts.filter((post) => post.pinned);
+    // Filtrer les articles épinglés à partir de tous les articles
+    const pinnedPosts = allPosts.filter((post) => post.pinned);
 
-	const loadMorePosts = () => {
-		setPage((prevPage) => prevPage + 1);
-	};
+    const loadMorePosts = () => {
+        setPage((prevPage) => prevPage + 1);
+    };
 
-	if (!allPosts || allPosts.length === 0)
-		return (
-			<div className="flex justify-center items-center h-screen">
-				<Spinner />
-			</div>
-		);
-	if (!heroPost && morePosts.length === 0)
-		return <div className="container mx-auto px-5 mt-20">No posts found.</div>;
+    if (!allPosts || allPosts.length === 0)
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <Spinner />
+            </div>
+        );
+    if (!heroPost && morePosts.length === 0)
+        return <div className="container mx-auto px-5 mt-20">No posts found.</div>;
 
-	return (
-		<main>
-			<div className="container mx-auto px-5 mt-20">
-				<div className="grid lg:grid-cols-3 gap-6 items-start">
-					{heroPost && (
-						<div className="lg:col-span-2">
-							<HeroPost
-								title={heroPost.title}
-								coverImage={heroPost.coverImage}
-								date={heroPost.date}
-								author={heroPost.author}
-								slug={heroPost.slug}
-								excerpt={heroPost.excerpt}
-							/>
-						</div>
-					)}
-					<div className="lg:col-span-1 flex flex-col gap-4">
-						{pinnedPosts.map((post) => (
-							<div key={post.slug} className=" w-full h-1/3">
-								<PostPreviewCompact
-									title={post.title}
-									coverImage={post.coverImage}
-									date={post.date}
-									author={post.author}
-									slug={post.slug}
-									excerpt={post.excerpt}
-								/>
-							</div>
-						))}
-					</div>
-				</div>
+    return (
+        <main>
+            <div className="container mx-auto px-5 mt-20">
+                <div className="grid lg:grid-cols-3 gap-6 items-start">
+                    {heroPost && (
+                        <div className="lg:col-span-2">
+                            <HeroPost
+                                title={heroPost.title}
+                                coverImage={heroPost.coverImage}
+                                date={heroPost.date}
+                                author={heroPost.author}
+                                slug={heroPost.slug}
+                                excerpt={heroPost.excerpt}
+                            />
+                        </div>
+                    )}
+                    <div className="lg:col-span-1 flex flex-col gap-4">
+                        {pinnedPosts.map((post) => (
+                            <div key={post.slug} className=" w-full h-1/3">
+                                <PostPreviewCompact
+                                    title={post.title}
+                                    coverImage={post.coverImage}
+                                    date={post.date}
+                                    author={post.author}
+                                    slug={post.slug}
+                                    excerpt={post.excerpt}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
-				{morePosts.length > 0 && <MorePosts posts={morePosts} />}
-				{hasMorePosts && (
-					<div className="flex justify-center my-5">
-						<Button color="primary" onPress={loadMorePosts}>
-							Voir plus
-						</Button>
-					</div>
-				)}
-			</div>
-		</main>
-	);
+                {morePosts.length > 0 && <MorePosts posts={morePosts} />}
+                {hasMorePosts && (
+                    <div className="flex justify-center my-5">
+                        <Button color="primary" onPress={loadMorePosts}>
+                            Voir plus
+                        </Button>
+                    </div>
+                )}
+            </div>
+        </main>
+    );
 }
